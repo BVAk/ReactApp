f98680a290f1134f49285f813f92ab072abd4d18
BVAk <bekkviktoria55@gmail.com>
2020-06-10 15:05:17 +0300

    9 - import export

---
+++ src/Header.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+const Header = () => {
+	return (<div>
+		<a> Home</a>
+		<a> News</a>
+		<a> Message</a>
+	</div> );
+	}
+	export default Header;
---
+++ src/Technologies.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+const Technologies = () => {
+	return (<div>
+	<ul>
+		<li> Home</li>
+		<li> News</li>
+		<li> Message</li>
+		</ul>
+	</div> );
+	}
+	export default Technologies;
